34f9301c02ee2f25ee85f6150b3be27183968982
Matthew J. Sahagian <msahagian@dotink.org>
2013-02-13 23:03:55 -0800

    Stricture matching on @return token parsing

--- src/TokenParsers/TokenReturn.php
+++ src/TokenParsers/TokenReturn.php
@@ -22,7 +22,7 @@
 	class TokenReturn
 	{
 		const REGEX_VALID = '/
-			void\s*$|^[a-zA-Z_\x7f-\xff][a-zA-Z0-9_\x7f-\xff]*\s+.+
+			^void\s*$|^(?:(?:\\\\)?[a-zA-Z_\x7f-\xff][a-zA-Z0-9_\x7f-\xff]*)+\s+.+$
 		/xi';
 
 
