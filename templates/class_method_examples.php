8e2218194d3cdd31883d256b625af92bd0e7cd3f
Matthew J. Sahagian <msahagian@dotink.org>
2013-10-23 23:17:57 -0700

    License consistency, few spacing fixes.

--- templates/class_method_examples.php
+++ templates/class_method_examples.php
@@ -8,16 +8,18 @@
 			$external_examples[] = $example;
 		} else {
 			?>
+
 ```php
 <?= trim(file_get_contents($this->get('example_root') . DIRECTORY_SEPARATOR . $example)) ?>
 
 
 ```
-
 			<?php
 		}
 	}
 ?>
+
+
 <?php
 	if (count($external_examples)) {
 		foreach ($external_examples as $example) {
